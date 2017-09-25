b2cd2b32c4b0d47939f862c45d60eb5e38e83663
shubhambatra3019 <31996668+shubhambatra3019@users.noreply.github.com>
2017-09-25 13:43:41 -0400

    first commit

---
+++ View Controllers/NewRunViewController.swift
@@ -0,0 +1,209 @@
+/**
+ * Copyright (c) 2017 Razeware LLC
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * Notwithstanding the foregoing, you may not use, copy, modify, merge, publish,
+ * distribute, sublicense, create a derivative work, and/or sell copies of the
+ * Software in any work that is designed, intended, or marketed for pedagogical or
+ * instructional purposes related to programming, coding, application development,
+ * or information technology.  Permission for such use, copying, modification,
+ * merger, publication, distribution, sublicensing, creation of derivative works,
+ * or sale is expressly withheld.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+
+import UIKit
+import CoreLocation
+import MapKit
+
+class NewRunViewController: UIViewController {
+  
+  @IBOutlet weak var launchPromptStackView: UIStackView!
+  @IBOutlet weak var dataStackView: UIStackView!
+  @IBOutlet weak var startButton: UIButton!
+  @IBOutlet weak var stopButton: UIButton!
+  @IBOutlet weak var distanceLabel: UILabel!
+  @IBOutlet weak var timeLabel: UILabel!
+  @IBOutlet weak var paceLabel: UILabel!
+  @IBOutlet weak var mapContainerView: UIView!
+  @IBOutlet weak var mapView: MKMapView!
+  
+  var run: Run?
+  let locationManager = LocationManager.shared
+  var seconds = 0
+  var timer: Timer?
+  var distance = Measurement(value: 0, unit: UnitLength.meters)
+  var locationList: [CLLocation] = []
+  
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    dataStackView.isHidden = true // required to work around behavior change in Xcode 9 beta 1
+  }
+  override func viewWillDisappear(_ animated: Bool) {
+    super.viewWillDisappear(animated)
+    timer?.invalidate()
+    locationManager.stopUpdatingLocation()
+  }
+  
+  @IBAction func startTapped() {
+    startRun()
+  }
+  
+  @IBAction func stopTapped() {
+    let alertController = UIAlertController(title: "End run?",
+                                            message: "Do you wish to end your run?",
+                                            preferredStyle: .actionSheet)
+    alertController.addAction(UIAlertAction(title: "Cancel", style: .cancel))
+    alertController.addAction(UIAlertAction(title: "Save", style: .default) { _ in
+      self.stopRun()
+      self.saveRun()
+      self.performSegue(withIdentifier: .details, sender: nil)
+    })
+    alertController.addAction(UIAlertAction(title: "Discard", style: .destructive) { _ in
+      self.stopRun()
+      _ = self.navigationController?.popToRootViewController(animated: true)
+    })
+    
+    present(alertController, animated: true)
+  }
+  
+  private func startRun() {
+    launchPromptStackView.isHidden = true
+    dataStackView.isHidden = false
+    startButton.isHidden = true
+    stopButton.isHidden = false
+    mapContainerView.isHidden = false
+    mapView.removeOverlays(mapView.overlays)
+    
+    seconds = 0
+    distance = Measurement(value: 0, unit: UnitLength.meters)
+    locationList.removeAll()
+    updateDisplay()
+    timer = Timer.scheduledTimer(withTimeInterval: 1.0, repeats: true) { _ in
+      self.eachSecond()
+    }
+    startLocationUpdates()
+  }
+  
+  private func stopRun() {
+    launchPromptStackView.isHidden = false
+    dataStackView.isHidden = true
+    startButton.isHidden = false
+    stopButton.isHidden = true
+    mapContainerView.isHidden = true
+    
+    locationManager.stopUpdatingLocation()
+  }
+  
+  func eachSecond() {
+    seconds += 1
+    updateDisplay()
+  }
+  
+  private func updateDisplay() {
+    let formattedDistance = FormatDisplay.distance(distance)
+    let formattedTime = FormatDisplay.time(seconds)
+    let formattedPace = FormatDisplay.pace(distance: distance,
+                                           seconds: seconds,
+                                           outputUnit: UnitSpeed.minutesPerMile)
+    
+    distanceLabel.text = "Distance:  \(formattedDistance)"
+    timeLabel.text = "Time:  \(formattedTime)"
+    paceLabel.text = "Pace:  \(formattedPace)"
+  }
+  
+  private func startLocationUpdates() {
+    locationManager.delegate = self
+    locationManager.activityType = .fitness
+    locationManager.distanceFilter = 10
+    locationManager.startUpdatingLocation()
+  }
+  
+  private func saveRun() {
+    let newRun = Run(context: CoreDataStack.context)
+    newRun.distance = distance.value
+    newRun.duration = Int16(seconds)
+    newRun.timestamp = Date() as NSDate
+    
+    for location in locationList {
+      let locationObject = Location(context: CoreDataStack.context)
+      locationObject.timestamp = location.timestamp as NSDate
+      locationObject.latitude = location.coordinate.latitude
+      locationObject.longitude = location.coordinate.longitude
+      newRun.addToLocations(locationObject)
+    }
+    
+    CoreDataStack.saveContext()
+    
+    run = newRun
+  }
+}
+
+// MARK: - Navigation
+
+extension NewRunViewController: SegueHandlerType {
+  enum SegueIdentifier: String {
+    case details = "RunDetailsViewController"
+  }
+  
+  override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+    switch segueIdentifier(for: segue) {
+    case .details:
+      let destination = segue.destination as! RunDetailsViewController
+      destination.run = run
+    }
+  }
+}
+
+// MARK: - Location Manager Delegate
+
+extension NewRunViewController: CLLocationManagerDelegate {
+  
+  func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+    for newLocation in locations {
+      let howRecent = newLocation.timestamp.timeIntervalSinceNow
+      guard newLocation.horizontalAccuracy < 20 && abs(howRecent) < 10 else { continue }
+      
+      if let lastLocation = locationList.last {
+        let delta = newLocation.distance(from: lastLocation)
+        distance = distance + Measurement(value: delta, unit: UnitLength.meters)
+        let coordinates = [lastLocation.coordinate, newLocation.coordinate]
+        mapView.add(MKPolyline(coordinates: coordinates, count: 2))
+        let region = MKCoordinateRegionMakeWithDistance(newLocation.coordinate, 500, 500)
+        mapView.setRegion(region, animated: true)
+      }
+      
+      locationList.append(newLocation)
+    }
+  }
+}
+
+// MARK: - Map View Delegate
+
+extension NewRunViewController: MKMapViewDelegate {
+  func mapView(_ mapView: MKMapView, rendererFor overlay: MKOverlay) -> MKOverlayRenderer {
+    guard let polyline = overlay as? MKPolyline else {
+      return MKOverlayRenderer(overlay: overlay)
+    }
+    let renderer = MKPolylineRenderer(polyline: polyline)
+    renderer.strokeColor = .blue
+    renderer.lineWidth = 3
+    return renderer
+  }
+}
